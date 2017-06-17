205a066ec12f24926db566e6c11f67be54003c55
Luan <luanpereirast@gmail.com>
2017-06-17 17:13:15 -0300

    Versões recursivas e iterativas das buscas concluídas

--- include/busca.h
+++ include/busca.h
@@ -17,14 +17,35 @@
 * @return boolean para o caso do elemento estar ou não no vetor
 */
 template <class T>
-bool buscaBinariaR(T *V, int N, T x){
-	if(N<=0) return false;
+int buscaBinariaR(T *V, int N, T x){
+	if(N<=0) return -1;
 	int k=N/2;
-	if (V[k]==x) return true;
+	if (V[k]==x) return k;
 	if (x<V[k]) return buscaBinariaR(V, k, x);
 	if (x>V[k]) return buscaBinariaR(&V[k+1], N-k-1, x);
 	
-	return false;
+	return -1;
+}
+
+/**
+* @brief Responsável por realizar uma busca binária e retornar boolean para caso o elemento buscado esteja ou não no vetor
+* @param *V vetor para realizar a busca
+* @param N Tamanho do vetor
+* @param x Elemento à ser buscado no vetor
+* @return boolean para o caso do elemento estar ou não no vetor
+*/
+template <class T>
+int buscaBinariaI(T *V, int N, T x){
+	int esq, mid, dir;                             
+  	esq = 0; 
+  	dir = N-1;                         
+   	while (esq <= dir) {                         
+    	mid = (esq + dir)/2;                        
+      	if (V[mid] == x) return mid;               
+      	if (V[mid] < x) esq = mid + 1;              
+      	else dir = mid - 1;                       
+   	}                                       
+   	return -1;        
 }
 
 
@@ -37,15 +58,43 @@ bool buscaBinariaR(T *V, int N, T x){
 * @return boolean para o caso do elemento estar ou não no vetor
 */
 template <class  T>
-int buscaTernariaR(T *V, int ini, int fim, T x){
+int buscaTernariaR(T *V, int fim, T x, int ini = 0){
 	int mid1=fim/3, mid2=2*fim/3;
-	if (x==V[ini]) return ini;
-	if (x==V[fim]) return fim; 
-	if (x>V[ini] and x<V[mid1]) return buscaTernariaR(V, ini+1, mid1-1, x);
-	if (x>=V[mid1] and x<=V[mid2]) return buscaTernariaR(V, mid1, mid2, x);
-	if (x>V[mid2] and x<V[fim]) return buscaTernariaR(V, mid2+1, fim-1, x);
+	if (x==V[mid1]) return mid1;
+	if (x==V[mid2]) return mid2; 
+	if (x>=V[ini] and x<V[mid1]) return buscaTernariaR(V, mid1-1, x, ini+1);
+	if (x>V[mid1] and x<V[mid2]) return buscaTernariaR(V, mid2, x, mid1);
+	if (x>=V[mid2] and x<=V[fim]) return buscaTernariaR(V, fim-1, x, mid2+1);
 	
 	return -1;
+}
+
+/**
+* @brief Responsável por realizar uma busca binária e retornar boolean para caso o elemento buscado esteja ou não no vetor
+* @param *V vetor para realizar a busca
+* @param N Tamanho do vetor
+* @param x Elemento à ser buscado no vetor
+* @return boolean para o caso do elemento estar ou não no vetor
+*/
+template <class T>
+int buscaTernariaI(T *V, int N, T x){
+	int esq, mid1 = 0, mid2 = 1, dir;                             
+  	esq = 0; 
+  	dir = N-1;                         
+   	while (esq <= dir) {                          
+    	mid1 = (dir - esq)/3 + esq;    
+    	mid2 = 2*(dir - esq)/3 + esq;                     
+      	if (V[mid1] == x) return mid1; 
+      	if (V[mid2] == x) return mid2;             
+      	if (x < V[mid1]) dir = mid1 - 1;  
+      	if (x > V[mid2]) esq = mid2 + 1;           
+      	if (V[mid1] < x and x < V[mid2]) {
+      		esq = mid1 + 1; 
+      		dir = mid2 - 1;   
+		}   
+		//cout << esq << " " << mid1 << " " << mid2 << " " << dir << endl;                 
+   	}                                         
+   	return -1;        
 }
 
 
@@ -61,6 +110,7 @@ template <class T>
 int buscaSequencialI(T *V, int N, T x){
 	for (int i=0; i<N; i++){
 		if(x==V[i]) return i;
+		if(x<V[i]) return -1;
 	}
 	
 	return -1;
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,42 @@ using std::endl;
 #include "pilha.h"
 
 int main(){
-	int V[5] = {1, 2, 3, 4, 5};
-	cout << buscaTernariaR(V, 0, 5, 4) << endl;
-
+	int V[5] = {1, 2, 4, 5, 8};
+	cout << "BBR: " << buscaBinariaR(V, 5, 0) << endl;
+	cout << "BBR: " << buscaBinariaR(V, 5, 1) << endl;
+	cout << "BBR: " << buscaBinariaR(V, 5, 3) << endl;
+	cout << "BBR: " << buscaBinariaR(V, 5, 4) << endl;
+	cout << "BBR: " << buscaBinariaR(V, 5, 9) << endl;
+	cout << endl;
+	cout << "BBI: " << buscaBinariaI(V, 5, 0) << endl;
+	cout << "BBI: " << buscaBinariaI(V, 5, 2) << endl;
+	cout << "BBI: " << buscaBinariaI(V, 5, 6) << endl;
+	cout << "BBI: " << buscaBinariaI(V, 5, 8) << endl;
+	cout << "BBI: " << buscaBinariaR(V, 5, 10) << endl;
+	cout << endl;
+	cout << "BSR: " << buscaSequencialR(V, 5, 0) << endl;
+	cout << "BSR: " << buscaSequencialR(V, 5, 1) << endl;
+	cout << "BSR: " << buscaSequencialR(V, 5, 3) << endl;
+	cout << "BSR: " << buscaSequencialR(V, 5, 4) << endl;
+	cout << "BSR: " << buscaSequencialR(V, 5, 9) << endl;
+	cout << endl;
+	cout << "BSI: " << buscaSequencialI(V, 5, 0) << endl;
+	cout << "BSI: " << buscaSequencialI(V, 5, 2) << endl;
+	cout << "BSI: " << buscaSequencialI(V, 5, 6) << endl;
+	cout << "BSI: " << buscaSequencialI(V, 5, 8) << endl;
+	cout << "BSI: " << buscaSequencialR(V, 5, 10) << endl;
+	cout << endl;
+	cout << "BTR: " << buscaTernariaR(V, 5, 0) << endl;
+	cout << "BTR: " << buscaTernariaR(V, 5, 1) << endl;
+	cout << "BTR: " << buscaTernariaR(V, 5, 3) << endl;
+	cout << "BTR: " << buscaTernariaR(V, 5, 4) << endl;
+	cout << "BTR: " << buscaTernariaR(V, 5, 9) << endl;
+	cout << endl;
+	cout << "BTI: " << buscaTernariaI(V, 5, 0) << endl;
+	cout << "BTI: " << buscaTernariaI(V, 5, 2) << endl;
+	cout << "BTI: " << buscaTernariaI(V, 5, 6) << endl;
+	cout << "BTI: " << buscaTernariaI(V, 5, 8) << endl;
+	cout << "BTI: " << buscaTernariaI(V, 5, 10) << endl;
+	cout << endl;
 	return 0;
 }
